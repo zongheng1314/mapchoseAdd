2c83e7516316c783a4f2120353fc6927e96ab0bb
tianhao <tianhao@tianhaodeMac.local>
2016-12-28 03:05:19 -0800

    Initial Commit

---
+++ baiduDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  baiduDemo
+//
+//  Created by tianhao on 2016/12/28.
+//  Copyright © 2016年 tianhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ baiduDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  baiduDemo
+//
+//  Created by tianhao on 2016/12/28.
+//  Copyright © 2016年 tianhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
